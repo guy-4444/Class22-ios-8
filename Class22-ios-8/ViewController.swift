baa0897488148cb73fcd45415c94e871e1661a79
guymacin <guyskv@gmail.com>
2022-05-23 18:32:43 +0300

    Initial Commit.

--- Class22-ios-8/ViewController.swift
+++ Class22-ios-8/ViewController.swift
@@ -1,19 +1,55 @@
-//
-//  ViewController.swift
-//  Class22-ios-8
-//
-//  Created by Guy Macin on 16/05/2022.
-//
-
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    
+    
+    let images: [String] = [
+        "https://firebasestorage.googleapis.com/v0/b/class-wedding.appspot.com/o/Shakespeare.jpeg?alt=media&token=ba6d2a6e-b3ac-4534-bd05-53fae668a02d",
+        "https://firebasestorage.googleapis.com/v0/b/class-wedding.appspot.com/o/img_girl.jpg?alt=media&token=fb11557d-994a-4cbb-8bc0-b814b0a95b92",
+        "https://firebasestorage.googleapis.com/v0/b/class-wedding.appspot.com/o/img_girl2.jpg?alt=media&token=ec7b8e23-b60f-4dac-bb72-a28f5220a071",
+        "https://upload.wikimedia.org/wikipedia/commons/f/f7/Crouching_Panda.jpg"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+//        image.layer.cornerRadius = 50.0
+//        image.layer.masksToBounds = true
+
+        let url = URL(string: images[2])
+        image.kf.setImage(with: url)
+        
+        image2.kf.setImage(with: url)
+        
+        let processor = DownsamplingImageProcessor(size: image2.bounds.size)
+                |> RoundCornerImageProcessor(cornerRadius: 140)
+        
+        KF.url(url)
+          .setProcessor(processor)
+          .loadDiskFileSynchronously()
+          .fade(duration: 0.25)
+          .set(to: image2)
+
     }
 
 
 }
 
+
+@IBDesignable extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set {
+              layer.cornerRadius = newValue
+
+              // If masksToBounds is true, subviews will be
+              // clipped to the rounded corners.
+              layer.masksToBounds = (newValue > 0)
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Class22-ios-8' do
+
+  pod 'ConfettiView'
+  pod 'Kingfisher', '~> 7.0'
+
+end
+
